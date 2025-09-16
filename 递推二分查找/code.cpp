eaab171def0d0da59b34157f5b3b8a64b813ed0d
Niobium-41-nb <1453070926@qq.com>
2025-09-16 11:26:56 +0800

    添加了一些代码喵

---
+++ 递推二分查找/code.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+const int maxn = 1e6;
+
+int A[maxn],x;
+
+int binarysearch(int low,int high){
+    if(low > high)return 0;
+    int mid = (low+high)/2;
+    if(x == A[mid])return mid;
+    else if (x < A[mid])return binarysearch(low,mid-1);
+    else binarysearch(mid+1,high);
+}
+
+int main(){
+    int n;
+    cin >> n >> x;
+    for(int i = 1;i <= n;i++){
+        cin >> A[i];
+    }
+
+    cout << binarysearch(1,n) << '\n';
+
+    return 0;
+}
